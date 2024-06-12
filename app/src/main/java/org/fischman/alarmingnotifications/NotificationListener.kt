00a7a1771fdbccc2aa18a7e9fa64368d84a632d9
Ami Fischman <ami@fischman.org>
2024-06-11 17:35:24 -0700

    Dismiss original notification when stopping the alarm, and give extra hints to promote the alarming notification to the top of the list

--- app/src/main/java/org/fischman/alarmingnotifications/NotificationListener.kt
+++ app/src/main/java/org/fischman/alarmingnotifications/NotificationListener.kt
@@ -1,5 +1,6 @@
 package org.fischman.alarmingnotifications
 
+import android.annotation.SuppressLint
 import android.app.AlarmManager
 import android.app.Notification
 import android.app.NotificationManager
@@ -29,6 +30,7 @@ class NotificationListener : NotificationListenerService() {
     override fun onCreate() {
         super.onCreate()
         mp.setDataSource(applicationContext, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM))
+        @Suppress("DEPRECATION")
         mp.setAudioStreamType(AudioManager.STREAM_ALARM)
         mp.isLooping = true
 
@@ -41,7 +43,7 @@ class NotificationListener : NotificationListenerService() {
         }
 
         // Other places that text can be stored in in Notifications. Possibly of future interest for apps other than GCal and GMail.
-        if (false) {
+        if (0 > 1) {
             val textFields = mutableListOf(
                 Notification.EXTRA_TITLE,
                 Notification.EXTRA_TITLE_BIG,
@@ -64,7 +66,7 @@ class NotificationListener : NotificationListenerService() {
             log("Full notification: $sbn")
             log("and extras: ")
             for (key in sbn.notification.extras.keySet()) {
-                log("key:"+key + ", value: " + sbn.notification.extras.getString(key))
+                log("key:" + key + ", value: " + sbn.notification.extras.getString(key))
             }
         }
 
@@ -82,33 +84,36 @@ class NotificationListener : NotificationListenerService() {
 
         val label = ((tickerText ?: "") + "\n" + (extraText ?: "") + "\n" + (titleText ?: "")).trim()
         log("onNotificationPosted: $label")
-        showNotification(label)
+        showNotification(label, sbn.key)
     }
 
-    private fun createPendingIntent(requestCode: Int, action: String, notificationID: Int, label: String): PendingIntent {
+    private fun createPendingIntent(requestCode: Int, action: String, notificationID: Int, label: String, originalNotificationKey: String): PendingIntent {
         val intent = Intent(this, NotificationListener::class.java)
         intent.putExtra("action", action)
         intent.putExtra("notificationID", notificationID)
         intent.putExtra("label", label)
+        intent.putExtra("originalNotificationKey", originalNotificationKey)
         return PendingIntent.getService(this, requestCode, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
     }
 
-    private fun showNotification(label: String) {
+    @Suppress("DEPRECATION")
+    private fun showNotification(label: String, originalNotificationKey: String) {
         mp.prepare()
         mp.start()
 
         val notificationID = Random.nextInt(0, 999999)
-        val stopIntent = createPendingIntent(0, "stop", notificationID, label)
-        val snooze1mIntent = createPendingIntent(1, "snooze1m", notificationID, label)
-        val snooze5mIntent = createPendingIntent(2, "snooze5m", notificationID, label)
+        val stopIntent = createPendingIntent(0, "stop", notificationID, label, originalNotificationKey)
+        val snooze1mIntent = createPendingIntent(1, "snooze1m", notificationID, label, originalNotificationKey)
+        val snooze5mIntent = createPendingIntent(2, "snooze5m", notificationID, label, originalNotificationKey)
 
         val notificationManager = getSystemService(NotificationManager::class.java)
         val notificationBuilder =
             Notification.Builder(this, notificationChannelID)
                 .setSmallIcon(android.R.drawable.ic_lock_idle_alarm)
+                .setVisibility(Notification.VISIBILITY_PUBLIC)
                 .setContentTitle(label)
                 .setContentText("")
-                .setCategory(Notification.CATEGORY_ALARM)
+                .setCategory(Notification.CATEGORY_CALL)
                 .setOngoing(true)
                 .setFlag(Notification.FLAG_NO_CLEAR, true)
                 .setDeleteIntent(stopIntent)
@@ -130,6 +135,7 @@ class NotificationListener : NotificationListenerService() {
     private fun bundleToString(bundle: Bundle?): String {
         if (bundle == null) return "(null bundle))"
         var str = "Bundle{"
+        @Suppress("DEPRECATION")
         for (key in bundle.keySet()) str += " $key: ${bundle[key]};"
         str += "}"
         return str
@@ -139,9 +145,10 @@ class NotificationListener : NotificationListenerService() {
         log("extras: ${bundleToString(intent?.extras)}")
         val action = intent?.getStringExtra("action") ?: return START_NOT_STICKY
         val label = intent.getStringExtra("label") ?: return START_NOT_STICKY
+        val originalNotificationKey = intent.getStringExtra("originalNotificationKey") ?: return START_NOT_STICKY
 
         if (action == "show") {
-            showNotification(label)
+            showNotification(label, originalNotificationKey)
             return START_NOT_STICKY
         }
 
@@ -149,15 +156,20 @@ class NotificationListener : NotificationListenerService() {
         if (notificationID < 0) return START_NOT_STICKY
 
         when (action) {
-            "stop" -> { dismiss(notificationID) }
-            "snooze1m", "snooze5m" -> { snooze(action, label, notificationID) }
+            "stop" -> { dismiss(notificationID, originalNotificationKey) }
+            "snooze1m", "snooze5m" -> { snooze(action, label, notificationID, originalNotificationKey) }
             else -> { Log.e("AMI","Unknown action: $action!") }
         }
         return START_NOT_STICKY
 
     }
 
-    private fun snooze(action: String, label: String, notificationID: Int) {
+    @SuppressLint("ScheduleExactAlarm")
+    private fun setExactAlarm(alarmManager: AlarmManager, aci: AlarmManager.AlarmClockInfo, pendingIntent: PendingIntent) {
+        alarmManager.setAlarmClock(aci, pendingIntent)
+    }
+
+    private fun snooze(action: String, label: String, notificationID: Int, originalNotificationKey: String) {
         log("snooze: $action $label $notificationID")
         val durStr = action.removePrefix("snooze")
         if (durStr == action) { Log.wtf("AMI", "Missing prefix 'snooze' in $action") }
@@ -166,21 +178,25 @@ class NotificationListener : NotificationListenerService() {
         val minutes = minutesStr.toInt()
         if (minutes != 5 && minutes != 1) { Log.wtf("AMI", "Unexpected snooze duration of $minutes in $action") }
 
-        dismiss(notificationID)
+        dismiss(notificationID, "")
 
         val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val intent = Intent(this, NotificationListener::class.java)
         intent.putExtra("action", "show")
         intent.putExtra("label", label)
+        intent.putExtra("originalNotificationKey", originalNotificationKey)
         val pendingIntent = PendingIntent.getService(this, 4, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
         val aci = AlarmManager.AlarmClockInfo(System.currentTimeMillis() + 60*1000*minutes, null)
         log("snoozed for $minutes minutes")
-        alarmManager.setAlarmClock(aci, pendingIntent)
+        setExactAlarm(alarmManager, aci, pendingIntent)
     }
 
-    private fun dismiss(notificationID: Int) {
+    private fun dismiss(notificationID: Int, originalNotificationKey: String) {
         log("dismiss: notificationID: $notificationID")
         if (mp.isPlaying) mp.stop()
         getSystemService(NotificationManager::class.java).cancel(notificationID)
+
+        cancelNotification(originalNotificationKey)
     }
+
 }
